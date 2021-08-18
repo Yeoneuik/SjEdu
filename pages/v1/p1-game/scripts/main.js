ae6b7086eaf4bf3ec5e1944f09a9a8ead22fe179
Ko Hyeongseok <kohs102@gmail.com>
2021-08-18 10:48:38 +0900

    디렉토리 구조 변경(v1)

--- pages/v1/p1-game/scripts/main.js
+++ pages/v1/p1-game/scripts/main.js
@@ -1,18 +1,23 @@
 const ansTable = {
+    // 정답칸 갯수
     numAnswer: 2,
+    // 선택지 갯수
     numObject: 6,
+    // 선택지 이미지 경로
     srcObject: {
-        0: 'sources/img/letter-1.svg',
-        1: 'sources/img/letter-2.svg',
-        2: 'sources/img/letter-3.svg',
-        3: 'sources/img/letter-4.svg',
-        4: 'sources/img/letter-5.svg',
-        5: 'sources/img/letter-6.svg',
+        0: 'assets/img/letter-1.svg',
+        1: 'assets/img/letter-2.svg',
+        2: 'assets/img/letter-3.svg',
+        3: 'assets/img/letter-4.svg',
+        4: 'assets/img/letter-5.svg',
+        5: 'assets/img/letter-6.svg',
     },
+    // 정답 이미지 경로
     srcAnswer: {
-        0: 'sources/img/letter-1.svg',
-        1: 'sources/img/letter-3.svg',
+        0: 'assets/img/letter-1.svg',
+        1: 'assets/img/letter-3.svg',
     },
+    // 정답별 올바른 선택지 번호
     dict: {
         0: 0,
         1: 2
@@ -23,6 +28,7 @@ const ansTable = {
     }
 }
 
+// 전역 드래그 기록 객체
 var nowDragging = {
     dragging: false,
     num: null,
@@ -51,7 +57,7 @@ function isEnded() {
 
 function rightAnswer(i) {
     console.log("Right Answer!");
-    var right = new Audio('sources/sound/right.mp3');
+    var right = new Audio('assets/sound/right.mp3');
     right.currentTime = 0.2;
     right.play();
     right.remove();
@@ -59,20 +65,16 @@ function rightAnswer(i) {
     getAnswer(i).style.opacity = 1;
     ansTable.isFilled[i] = true;
     if (isEnded()) {
-        
-        
-        
-        setTimeout(function(){
-                   document.getElementById("endedDialog").style.display = "block";
-                document.getElementById("endedFocus").style.display = "block";
-                   
-                   }, 2000)
+        setTimeout(function () {
+            document.getElementById("endedDialog").style.display = "block";
+            document.getElementById("endedFocus").style.display = "block";
+        }, 2000)
     }
 }
 
 function wrongAnswer() {
     console.log("Wrong Answer!");
-    var wrong = new Audio('sources/sound/wrong.mp3');
+    var wrong = new Audio('assets/sound/wrong.mp3');
     wrong.currentTime = 0.4;
     wrong.play();
     wrong.remove();
@@ -94,7 +96,7 @@ function inAnswer() {
 function onDesktop() {
     for (let i = 0; i < ansTable.numObject; i++) {
         let nowObject = getObject(i);
-        nowObject.ondragstart = function() { return false; }
+        nowObject.ondragstart = function () { return false; }
         nowObject.onmousedown = function (e) {
             e.preventDefault();
 
@@ -121,8 +123,8 @@ function onDesktop() {
         }
     }
 
-    document.onmousemove = function(e) {
-        if(nowDragging.dragging) {
+    document.onmousemove = function (e) {
+        if (nowDragging.dragging) {
             var grabLocation = e;
             nowDragging.obj.style.left = grabLocation.clientX - nowDragging.offset.x + 'px';
             nowDragging.obj.style.top = grabLocation.clientY - nowDragging.offset.y + 'px';
@@ -130,8 +132,8 @@ function onDesktop() {
         }
     }
 
-    document.onmouseup = function(e) {
-        if(nowDragging.dragging) {
+    document.onmouseup = function (e) {
+        if (nowDragging.dragging) {
             nowDragging.dragging = false;
             var ans = inAnswer();
             if (ans != null) {
